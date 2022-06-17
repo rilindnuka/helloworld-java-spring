cd3d0837f2d441257445d2fb163bb2d6a7182ed1
Rilind Nuka <rilindnuka@gmail.com>
2022-06-17 16:19:41 +0200

    Initial commit

---
+++ src/main/java/com/example/helloworld/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
